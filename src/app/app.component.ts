a9d41156095c96ddb628253709282df60262275f
AnkushT27 <ankushtiwari102@gmail.com>
2021-01-22 10:01:23 +0530

    removed all extra modules and added the rxjs timeout operator to handle timeout on client end to show something went wrong

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 import { AppService } from './app.service';
-import { take } from 'rxjs/operators';
+import { take, timeout } from 'rxjs/operators';
+import { Observable, Subscriber, Subscription } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit,OnDestroy{
   task:Array<String>;
   tasksSelected:Array<String>;
+  subscription:Subscription;
+  errorFlag:Boolean;
   constructor(private taskService:AppService){
     this.task=[];
     this.tasksSelected = [];
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()
   }
  
   ngOnInit(): void {
@@ -22,8 +28,11 @@ export class AppComponent implements OnInit{
   
   getTasks(){
     //to unsubscribe after one stream is initiated
-    this.taskService.getData().pipe(take(1)).subscribe(({data}:any)=>{
+    //added a timeout set to 10 seconds if api fails to respond would fallback into the err callback
+    this.subscription = this.taskService.getData().pipe(take(1),timeout(10000)).subscribe(({data}:any)=>{
         this.task = data;
+    },(err)=>{
+        this.errorFlag=true;
     })
   }
 
@@ -31,7 +40,7 @@ export class AppComponent implements OnInit{
 
   drop(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.task, event.previousIndex, event.currentIndex);
-    //moveItemInArray(this.moviesSelected, event.previousIndex, event.currentIndex);
+    //moveItemInArray(this.tasksSelected, event.previousIndex, event.currentIndex);
     this.reorderData();
     
   }
